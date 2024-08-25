a97f6901989b5ea3773345b33fef5df34c59e44e
Nelson Cadenas <nca1478@yahoo.com>
2024-08-24 20:16:28 -0400

    Configurar OrdersMicroservice

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main');
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: envs.host,
+        port: envs.port,
+        retryAttempts: envs.retryAttempts || 5,
+        retryDelay: envs.retryDelay || 3000,
+      },
+    },
+  );
 
-  await app.listen(envs.port);
+  await app.listen();
 
   logger.log(`Orders Microservice running on port ${envs.port}`);
 }
