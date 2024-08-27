b29e705db0e7fe023db51678d25cb8b43d5707d9
Nelson Cadenas <nca1478@yahoo.com>
2024-08-27 15:20:28 -0400

    Tarea - Conectar OrdersMS y ProductsMS de TCP a NATS (agregado transports)

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,21 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { envs, NATS_SERVICE } from '../config';
+import { NatsModule } from '../transports/nats.module';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS,
-        options: {
-          servers: envs.natsServers,
-        },
-      },
-    ]),
-  ],
+  imports: [NatsModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
