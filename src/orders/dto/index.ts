40b3f8d51cf4665a33e92107a7940197688105c7
Nelson Cadenas <nca1478@yahoo.com>
2024-08-30 01:25:59 -0400

    Preparar base de datos y PaidOrderDto

--- src/orders/dto/index.ts
+++ src/orders/dto/index.ts
@@ -2,3 +2,4 @@ export * from './create-order.dto';
 export * from './pagination-order.dto';
 export * from './change-status-order.dto';
 export * from './order-item.dto';
+export * from './paid-order.dto';
